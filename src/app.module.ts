7343f1dcad97a1f2be93025dc67d1ed596eff521
MoGamal27 <113600854+MoGamal27@users.noreply.github.com>
2024-08-14 10:03:55 +0300

    fix some configration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { DatabaseModule } from './config/databaseConfig';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BranchModule } from './branch/branch.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, BranchModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345',
+      database: 'Easy-book',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Set to false in production
+    }),
+    BranchModule,
+  ],
 })
 export class AppModule {}
-
