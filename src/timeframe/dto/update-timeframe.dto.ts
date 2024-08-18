3e4acc947da4a3f9650a81e779c135d102982f67
Mahmoud3mmar <55957447+Mahmoud3mmar@users.noreply.github.com>
2024-08-18 19:35:49 +0300

    Delete src directory

--- src/timeframe/dto/update-timeframe.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTimeFrameDto } from './create-timeframe.dto';
-
-export class UpdateTimeFrameDto extends PartialType(CreateTimeFrameDto) {}
