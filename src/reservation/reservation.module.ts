511975ef7ca3a34c2a04a6c691ed0d3a2e31108a
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-08-19 18:28:59 +0300

    feat : create reservation crud controllers/services

--- src/reservation/reservation.module.ts
+++ src/reservation/reservation.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReservationService } from './reservation.service';
 import { ReservationController } from './reservation.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReservationEntity } from './entities/reservation.entity';
+import { BranchEntity } from '../branch/entities/branch.entity';
+import { ServiceEntity } from '../service/entities/service.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReservationEntity,BranchEntity,ServiceEntity])],
   controllers: [ReservationController],
   providers: [ReservationService],
 })
