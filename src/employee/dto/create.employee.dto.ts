eaa9d6ecbecd765fa07e3f4ceea76ebfc836b94b
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-08-19 18:26:39 +0300

    feat : create employee dto

--- src/employee/dto/create.employee.dto.ts
+++ src/employee/dto/create.employee.dto.ts
@@ -1,28 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsUUID, IsNotEmpty } from 'class-validator';
 
 export class CreateEmployeeDto {
 
-  
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   english_Name: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   arabic_Name: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   employee_Type_English: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   employee_Type_Arabic: string;
 
+  @ApiProperty()
   @IsUUID()
   @IsNotEmpty()
   branch: string;
-
+  
+  @ApiProperty()
   @IsUUID()
   @IsNotEmpty()
   position: string;
