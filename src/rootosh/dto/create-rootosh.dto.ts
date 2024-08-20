282d3e56962e5cfeaa32b9714597adc368b99967
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-08-20 17:18:10 +0300

    feat : create rootosh dto

--- src/rootosh/dto/create-rootosh.dto.ts
+++ src/rootosh/dto/create-rootosh.dto.ts
@@ -1,1 +1,19 @@
-export class CreateRootoshDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateRootoshDto {
+  @ApiProperty({ description: 'The name of the rootosh' })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({ description: 'The description of the rootosh' })
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @ApiProperty({ description: 'The ID of the related service' })
+  @IsString()
+  @IsNotEmpty()
+  serviceId: string; // Assuming service ID is a string
+}
