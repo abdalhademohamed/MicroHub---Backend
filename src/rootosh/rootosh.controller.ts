c60f984210e5f96e228a2e49557f1e59a3ee12c4
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-08-20 17:18:48 +0300

    feat : implement  crud rootosh services/controllers

--- src/rootosh/rootosh.controller.ts
+++ src/rootosh/rootosh.controller.ts
@@ -1,34 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { RootoshService } from './rootosh.service';
+import { Controller, Get, Post, Body, Patch, Param, Put, Delete } from '@nestjs/common';
 import { CreateRootoshDto } from './dto/create-rootosh.dto';
+import { RootoshEntity } from './entities/rootosh.entity';
+import { RootoshService } from './rootosh.service';
 import { UpdateRootoshDto } from './dto/update-rootosh.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('rootosh')
 @Controller('rootosh')
 export class RootoshController {
-  constructor(private readonly rootoshService: RootoshService) {}
+  
+  private readonly RootoshService: RootoshService
 
-  // @Post()
-  // create(@Body() createRootoshDto: CreateRootoshDto) {
-  //   return this.rootoshService.create(createRootoshDto);
-  // }
+  @Post()
+  async create(@Body() createRootoshDto: CreateRootoshDto): Promise<RootoshEntity> {
+    return this.RootoshService.createRootosh(createRootoshDto);
+  }
 
-  // @Get()
-  // findAll() {
-  //   return this.rootoshService.findAll();
-  // }
+  @Get()
+  async findAll(): Promise<RootoshEntity[]> {
+    return this.RootoshService.findAllRootosh();
+  }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.rootoshService.findOne(+id);
-  // }
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<RootoshEntity> {
+    return this.RootoshService.findOneRootosh(id);
+  }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateRootoshDto: UpdateRootoshDto) {
-  //   return this.rootoshService.update(+id, updateRootoshDto);
-  // }
+  @Put(':id')
+  async update(
+    @Param('id',) id: number,
+    @Body() updateRootoshDto: UpdateRootoshDto,
+  ): Promise<RootoshEntity> {
+    return this.RootoshService.updateRootosh(id, updateRootoshDto);
+  }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.rootoshService.remove(+id);
-  // }
+  @Delete(':id')
+  async remove(@Param('id') id: number): Promise<void> {
+    return this.RootoshService.removeRootosh(id);
+  }
 }
