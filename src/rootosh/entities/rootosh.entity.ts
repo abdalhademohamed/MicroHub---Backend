c49e110932670e8b9b620ae85df4fc42d4431453
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-08-22 14:02:36 +0300

    refactor : rootosh entity

--- src/rootosh/entities/rootosh.entity.ts
+++ src/rootosh/entities/rootosh.entity.ts
@@ -3,8 +3,8 @@ import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 
 @Entity()
 export class RootoshEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Column()
   name: string;
