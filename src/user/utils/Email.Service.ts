7244f59bf6f5f2a004e7a3c4f55971df577a1fe4
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-08-26 11:14:54 +0300

    fix : mail service in deployment

--- src/user/utils/Email.Service.ts
+++ src/user/utils/Email.Service.ts
@@ -8,6 +8,8 @@ export class MailService {
     constructor() {
         this.transporter = nodemailer.createTransport({
             host: process.env.NodeMailer_HOST,
+            service: 'gmail', // Using Gmail service
+
             // port: 587,
             auth: {
                 user: process.env.NodeMailer_USER,
