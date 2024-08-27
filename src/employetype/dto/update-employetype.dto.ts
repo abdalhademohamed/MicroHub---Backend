0de410b8b42c812d412a221f34ea361346356964
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-08-27 13:50:25 +0300

    feat : create employee Type dto& entity

---
+++ src/employetype/dto/update-employetype.dto.ts
@@ -0,0 +1,11 @@
+import { IsOptional, IsString } from 'class-validator';
+
+export class UpdateEmployeeTypeDto {
+  @IsOptional()
+  @IsString()
+  readonly typeEnglish?: string;
+
+  @IsOptional()
+  @IsString()
+  readonly typeArabic?: string;
+}
