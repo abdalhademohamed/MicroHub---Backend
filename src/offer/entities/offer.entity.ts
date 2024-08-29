a6d1e99c8ed1c92df69e51859d36f60c4e12e3d5
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-08-29 11:53:44 +0300

    feat : create offer entity&dto

--- src/offer/entities/offer.entity.ts
+++ src/offer/entities/offer.entity.ts
@@ -1,1 +1,37 @@
-export class Offer {}
+import { BranchEntity } from "src/branch/entities/branch.entity";
+import { ServiceEntity } from "src/service/entities/service.entity";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToMany,
+  JoinTable,
+} from "typeorm";
+
+@Entity()
+export class OfferEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "varchar", length: 255 })
+  offerName: string;
+
+ 
+
+  @Column({ type: "timestamp" })
+  startDateTime: Date;
+
+  @Column({ type: "timestamp" })
+  endDateTime: Date;
+
+  @ManyToMany(() => ServiceEntity)
+  @JoinTable()
+  services: ServiceEntity[];
+
+  @ManyToMany(() => BranchEntity)
+  @JoinTable()
+  branches: BranchEntity[];
+
+  @Column({ type: "decimal", precision: 5, scale: 2 })
+  discountPercentage: number;
+}
