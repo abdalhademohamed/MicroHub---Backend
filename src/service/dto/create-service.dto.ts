119f46d34a6320299bfde014664d80f97f3c6686
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-08-29 11:55:52 +0300

    refactor : service dto

--- src/service/dto/create-service.dto.ts
+++ src/service/dto/create-service.dto.ts
@@ -1,42 +1,46 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty, IsNumber, IsPositive, isUUID, IsUUID, IsOptional, isString } from 'class-validator';
+import { IsString, IsNotEmpty, IsNumber, IsPositive, IsUUID, IsOptional, IsInt } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CreateServiceDto {
   @ApiProperty({ description: 'The URL of the service image' })
-  
   image?: string;
 
   @ApiProperty({ description: 'The name of the service in Arabic' })
   @IsString()
-  @IsOptional()  // If not always present
+  @IsOptional()
   arabic_Name: string;
 
   @ApiProperty({ description: 'The name of the service in English' })
-  // @IsString()
-  @IsOptional()  // If not always present
+  @IsString()
+  @IsOptional()
   english_Name: string;
 
   @ApiProperty({ description: 'The price of the service' })
-  // @IsString()
-  // @IsPositive({ message: 'Price must be a positive number' })
-  @IsOptional()  // If not always present
+  @IsNumber() 
+  @IsPositive({ message: 'Price must be a positive number' })
+  @IsOptional()
+  @Transform(({ value }) => parseFloat(value)) // Convert string to float
   price: number;
 
   @ApiProperty({ description: 'The duration of the service in minutes' })
-  // @IsString()
-  // @IsPositive({ message: 'Duration must be a positive number' })
-  @IsOptional()  // If not always present
+  @IsInt()
+  @IsPositive({ message: 'Duration must be a positive number' })
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value, 10)) // Convert string to integer
   duration_Mins: number;
 
   @ApiProperty({ description: 'The rootosh number of the service' })
-  // @IsString()
-  // @IsPositive({ message: 'Rootosh number must be a positive number' })
-  @IsOptional()  // If not always present
+  @IsInt()
+  @IsPositive({ message: 'Rootosh number must be a positive number' })
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value, 10)) // Convert string to integer
   rootosh_Number: number;
 
   @ApiProperty({ description: 'The number of months before the service expires' })
-  // @IsString()
-  // @IsPositive({ message: 'Months to expire must be a positive number' })
-  @IsOptional()  // If not always present
+  @IsInt()
+  @IsPositive({ message: 'Months to expire must be a positive number' })
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value, 10)) // Convert string to integer
   months_To_Expire: number;
 }
