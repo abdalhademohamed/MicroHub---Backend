54d983751a668689a4dee455e00804a25b054f87
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-02 12:06:14 +0300

    refactor : customer entity & dto

--- src/customer/dto/create.customer.dto.ts
+++ src/customer/dto/create.customer.dto.ts
@@ -16,23 +16,25 @@ export class CreateCustomerDto {
   @IsNotEmpty()
   fullName: string;
 
-  @IsNumber()
-  @Min(1)
-  @Max(31)
-  @Type(() => Number) // Transform the form-data string into a number
-  day: number;
+  // @IsNumber()
+  // @Min(1)
+  // @Max(31)
+  // @Type(() => Number) // Transform the form-data string into a number
+  // day: number;
 
-  @IsNumber()
-  @Min(1)
-  @Max(12)
-  @Type(() => Number) // Transform the form-data string into a number
-  month: number;
+  // @IsNumber()
+  // @Min(1)
+  // @Max(12)
+  // @Type(() => Number) // Transform the form-data string into a number
+  // month: number;
 
-  @IsNumber()
-  @Min(1900)
-  @Type(() => Number) // Transform the form-data string into a number
-  year: number;
-
+  // @IsNumber()
+  // @Min(1900)
+  // @Type(() => Number) // Transform the form-data string into a number
+  // year: number;
+  @IsString()
+  @IsNotEmpty()
+  dateOfBirth: string; // Format: YYYY-MM-DD
 
   @IsOptional()
   @IsISO8601({}, { message: 'customStartTime must be a valid ISO 8601 date string' })
