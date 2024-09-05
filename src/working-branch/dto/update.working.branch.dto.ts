571d0d62af7f4466d2d765f268ee3ceb9dfa0374
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-05 14:31:15 +0300

    feat : update working branch dto

--- src/working-branch/dto/update.working.branch.dto.ts
+++ src/working-branch/dto/update.working.branch.dto.ts
@@ -1,4 +1,18 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateWorkingBranchDto } from './create.working.branch.dto';
+import { IsEnum, IsString, IsUUID, IsArray, ArrayNotEmpty } from 'class-validator';
+import { WeekDays } from '../../branch/utils/days.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateWorkingBranchDto extends PartialType(CreateWorkingBranchDto) {}
+export class UpdateWorkingBranchDto {
+  
+  @ApiProperty({ description: 'The day of the week' })
+  @IsEnum(WeekDays)
+  dayOfWeek: WeekDays;
+
+
+
+  @ApiProperty({ description: 'Array of working hours for the day', example: ['09:00', '10:00', '11:00'] })
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
+  workingHours: string[];
+}
