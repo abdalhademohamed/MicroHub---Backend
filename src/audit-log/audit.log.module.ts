85cb794e1c543a51aad0eb334cb81b6006972d00
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-09 18:35:56 +0300

    refactor : audit log logic

---
+++ src/audit-log/audit.log.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AuditLogService } from './audit.log.service';
+import { AuditLogController } from './audit.log.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuditLogEntity } from './entities/audit.log.entity';
+
+@Module({
+  imports: [
+   
+  TypeOrmModule.forFeature([AuditLogEntity]) // Register audit log entity for other modules
+  ],
+  controllers: [AuditLogController],
+  providers: [AuditLogService],
+})
+export class AuditLogModule {} 
