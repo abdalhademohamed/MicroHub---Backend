9ca79ced07fe92b7b3c93e4371eb2a9f7ed04aef
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-09 19:38:16 +0300

    refactor :  branch dto

--- src/branch/dto/filter.branch.dto.ts
+++ src/branch/dto/filter.branch.dto.ts
@@ -1,46 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString, IsInt, Min } from 'class-validator';
-import { BranchEntity } from '../entities/branch.entity';
+import { IsOptional, IsString, IsInt, Min, IsEnum } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class FilterBranchesDto {
   @ApiProperty({ description: 'Page number for pagination', example: 1 })
+  @IsOptional()
   @IsInt()
   @Min(1)
-  page: number;
+  @Transform(({ value }) => Number(value), { toClassOnly: true })
+  page?: number;
 
   @ApiProperty({ description: 'Number of items per page', example: 10 })
+  @IsOptional()
   @IsInt()
   @Min(1)
-  limit: number;
+  @Transform(({ value }) => Number(value), { toClassOnly: true })
+  limit?: number;
 
-  @ApiProperty({ description: 'Branch name to filter by', required: false })
+  @ApiProperty({ description: 'Order of sorting by branch name', example: 'ASC', enum: ['ASC', 'DESC'], required: false })
   @IsOptional()
   @IsString()
-  name?: string;
-
-  @ApiProperty({ description: 'Location to filter by', required: false })
-  @IsOptional()
-  @IsString()
-  location?: string;
-
-  @ApiProperty({ description: 'Branch ID to filter by', required: false })
-  @IsOptional()
-  @IsString()
-  branchId?: string;
-
-  @ApiProperty({ description: 'List of branches matching the criteria', type: [BranchEntity], required: false })
-  @IsOptional()
-  items?: BranchEntity[];
-
-  @ApiProperty({ description: 'Total number of branches matching the criteria', required: false })
-  @IsOptional()
-  total?: number;
-
-  @ApiProperty({ description: 'Current page number', required: false })
-  @IsOptional()
-  currentPage?: number;
-
-  @ApiProperty({ description: 'Total number of pages', required: false })
-  @IsOptional()
-  totalPages?: number;
+  @IsEnum(['ASC', 'DESC'])
+  order?: 'ASC' | 'DESC';
 }
