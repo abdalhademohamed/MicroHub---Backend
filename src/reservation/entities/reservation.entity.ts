21d7fed8a99905c01025fe43413ad3ca7a5895c9
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-10 18:25:54 +0300

    refactor : add reservation entity

--- src/reservation/entities/reservation.entity.ts
+++ src/reservation/entities/reservation.entity.ts
@@ -1,21 +1,23 @@
 import { ServiceEntity } from '../../service/entities/service.entity';
 import { BranchEntity } from '../../branch/entities/branch.entity';
-import { Entity, Column, ManyToOne, PrimaryGeneratedColumn, ManyToMany } from 'typeorm';
+import { Entity, Column, ManyToOne, PrimaryGeneratedColumn, ManyToMany, OneToMany } from 'typeorm';
 import { EmployeeEntity } from '../../employee/entities/employee.entity';
+import { CustomerEntity } from '../../customer/entities/customer.entity';
+import { OrderEntity } from '../../orders/entities/order.entity';
 
 @Entity() // Specify table name if necessary
 export class ReservationEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ type: 'varchar', nullable: true }) // Ensure nullable is set if the column can have NULL values
-  country_Code: string;
+  // @Column({ type: 'varchar', nullable: true }) // Ensure nullable is set if the column can have NULL values
+  // country_Code: string;
 
-  @Column()
-  phone_Number: string; // Correct property name
+  // @Column()
+  // phone_Number: string; // Correct property name
 
-  @Column()
-  client_FullName: string; // Correct property name
+  // @Column()
+  // client_FullName: string; // Correct property name
 
   // @Column()
   // day: number; // User's birthday day
@@ -27,8 +29,8 @@ export class ReservationEntity {
   // year: number; // User's birthday year
 
 
-  @Column({ type: 'date' })
-  dateOfBirth: string; // Format: YYYY-MM-DD
+  // @Column({ type: 'date' })
+  // dateOfBirth: string; // Format: YYYY-MM-DD
   
   @Column()
   reservationDay: number; // Day of reservation
@@ -43,7 +45,11 @@ export class ReservationEntity {
   @Column({ type: 'timestamp' })
   end_Time: Date;
  
+  @Column({ type: 'number'})
+  totalPrice: number;
 
+  @Column({ type: 'number' })
+  deposit: number;
   @Column()
   deposit_Content: string; // Correct property name
 
@@ -53,9 +59,14 @@ export class ReservationEntity {
   @ManyToMany(() => ServiceEntity, (service) => service.reservations)
   services: ServiceEntity[]; // Handle multiple services
   // New relation to employees
-  // @ManyToMany(() => EmployeeEntity, (EmployeeEntity) => EmployeeEntity.reservations)
-  // employees: EmployeeEntity[];
+  @ManyToMany(() => EmployeeEntity, (EmployeeEntity) => EmployeeEntity.reservations)
+  employees: EmployeeEntity[];
 
-  // @ManyToOne(() => EmployeeEntity, { nullable: true })
-  // artist?: EmployeeEntity; // Ensure this relationship is defined
+  
+  @ManyToOne(() => CustomerEntity, customer => customer.reservations)
+  customer: CustomerEntity; // Relationship to CustomerEntity
+
+
+  @OneToMany(() => OrderEntity, OrderEntity => OrderEntity.reservation)
+  orders: OrderEntity[];
 }
