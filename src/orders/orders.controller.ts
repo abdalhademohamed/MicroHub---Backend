f3b02fbb6ba2731794ca5f921336514c6864ce08
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-10 18:25:41 +0300

    refactor : add orders logic

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,59 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UseGuards, UploadedFile, Query, BadRequestException, Put } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AccessTokenGuard } from '../auth/guards/accessToken.guard';
+import { RolesGuard } from '../auth/guards/role.guards';
+import { Roles } from '../auth/Roles.decorator';
+import { Role } from '../user/utils/user.enum';
+import { OrderEntity } from './entities/order.entity';
+import { FindOrdersDto } from './dto/find.all.orders.dto';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Post(':reservationId')
+  @ApiOperation({ summary: 'Create an order from a reservation' })
+  @ApiResponse({ status: 201, description: 'Order created successfully.' })
+  @ApiResponse({ status: 404, description: 'Reservation not found.' })
+  async createOrder(
+    @Param('reservationId') reservationId: string
+  ) {
+    return this.ordersService.createOrder(reservationId);
+  }
+
+
+
+
+  @Put('assign')
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.BRANCHMANAGER)
+  @ApiOperation({ summary: 'Assign an order to an artist' })
+  @ApiResponse({ status: 200, description: 'Order assigned successfully.', type: OrderEntity })
+  @ApiResponse({ status: 400, description: 'Bad request, orderId or artistId is missing.' })
+  @ApiResponse({ status: 404, description: 'Order or artist not found, or artist is not an artist.' })
+  async assignOrderToArtist(
+    @Param('orderId') orderId: string,
+    @Param('artistId') artistId: string
+  ): Promise<OrderEntity> {
+    if (!orderId || !artistId) {
+      throw new BadRequestException('Both orderId and artistId are required');
+    }
+    return this.ordersService.assignOrderToArtist(orderId, artistId);
+  }
+
+
+
+  @Get('sorted')
+  @ApiOperation({ summary: 'Get all orders with pagination, sorting, and filtering' })
+  @ApiResponse({ status: 200, description: 'Orders retrieved successfully.' })
+  @ApiResponse({ status: 500, description: 'Internal server error.' })
+  async getAllOrders(
+    @Query() findOrdersDto: FindOrdersDto
+  ): Promise<{ orders: OrderEntity[]; total: number }> {
+    return this.ordersService.findAllOrders(findOrdersDto);
+  }
+}
---
+++ src/orders/orders.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { OrdersController } from './orders.controller';
+import { OrderEntity } from './entities/order.entity';
+import { CommentEntity } from '../comment/entities/comment.entity';
+import { ReservationEntity } from '../reservation/entities/reservation.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CloudinaryService } from '../cloudinary/cloudinary.service';
+import { EmployeeEntity } from '../employee/entities/employee.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([OrderEntity, CommentEntity, ReservationEntity,EmployeeEntity]),
+  ],
+  controllers: [OrdersController],
+  providers: [OrdersService,CloudinaryService],
+})
+export class OrdersModule {}
