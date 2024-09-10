d4067b2c2c6d4fbed548213988dcc26c2c16cb3a
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-10 18:36:28 +0300

    refactor : reservation entity

--- src/reservation/entities/reservation.entity.ts
+++ src/reservation/entities/reservation.entity.ts
@@ -1,13 +1,20 @@
-import { ServiceEntity } from '../../service/entities/service.entity';
-import { BranchEntity } from '../../branch/entities/branch.entity';
-import { Entity, Column, ManyToOne, PrimaryGeneratedColumn, ManyToMany, OneToMany } from 'typeorm';
-import { EmployeeEntity } from '../../employee/entities/employee.entity';
-import { CustomerEntity } from '../../customer/entities/customer.entity';
-import { OrderEntity } from '../../orders/entities/order.entity';
+import { ServiceEntity } from "../../service/entities/service.entity";
+import { BranchEntity } from "../../branch/entities/branch.entity";
+import {
+  Entity,
+  Column,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  ManyToMany,
+  OneToMany,
+} from "typeorm";
+import { EmployeeEntity } from "../../employee/entities/employee.entity";
+import { CustomerEntity } from "../../customer/entities/customer.entity";
+import { OrderEntity } from "../../orders/entities/order.entity";
 
 @Entity() // Specify table name if necessary
 export class ReservationEntity {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
   // @Column({ type: 'varchar', nullable: true }) // Ensure nullable is set if the column can have NULL values
@@ -28,10 +35,9 @@ export class ReservationEntity {
   // @Column()
   // year: number; // User's birthday year
 
-
   // @Column({ type: 'date' })
   // dateOfBirth: string; // Format: YYYY-MM-DD
-  
+
   @Column()
   reservationDay: number; // Day of reservation
   @Column()
@@ -39,18 +45,17 @@ export class ReservationEntity {
   @Column()
   reservationYear: number; // Year of reservation
 
-  @Column({ type: 'timestamp' })
+  @Column({ type: "timestamp" })
   start_Time: Date;
 
-  @Column({ type: 'timestamp' })
+  @Column({ type: "timestamp" })
   end_Time: Date;
- 
-  @Column({ type: 'numeric', precision: 10, scale: 2, default: 0 })
+
+  @Column({ type: "numeric", precision: 10, scale: 2, default: 0 })
   totalPrice: number; // Total price as a numeric value with 2 decimal places
 
-  
-  @Column({ type: 'number' })
-  deposit: number;
+  @Column({ type: "numeric", precision: 10, scale: 2, default: 0 })
+  deposit: number; // Numeric value with 2 decimal places
   @Column()
   deposit_Content: string; // Correct property name
 
@@ -60,14 +65,15 @@ export class ReservationEntity {
   @ManyToMany(() => ServiceEntity, (service) => service.reservations)
   services: ServiceEntity[]; // Handle multiple services
   // New relation to employees
-  @ManyToMany(() => EmployeeEntity, (EmployeeEntity) => EmployeeEntity.reservations)
+  @ManyToMany(
+    () => EmployeeEntity,
+    (EmployeeEntity) => EmployeeEntity.reservations
+  )
   employees: EmployeeEntity[];
 
-  
-  @ManyToOne(() => CustomerEntity, customer => customer.reservations)
+  @ManyToOne(() => CustomerEntity, (customer) => customer.reservations)
   customer: CustomerEntity; // Relationship to CustomerEntity
 
-
-  @OneToMany(() => OrderEntity, OrderEntity => OrderEntity.reservation)
+  @OneToMany(() => OrderEntity, (OrderEntity) => OrderEntity.reservation)
   orders: OrderEntity[];
 }
