9ecbf7fc883a41882543fdd5d10c892a977de8a1
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-12 12:46:44 +0300

    refactor : user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,57 +1,62 @@
-
-import { Column, Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import { Role } from "../utils/user.enum";
 import { NotificationEntity } from "../../notification/entities/notification.entity";
 import { FcmTokenEntity } from "../../notification/entities/fcm.token.entity";
-
+import { BranchEntity } from "src/branch/entities/branch.entity";
 
 @Entity()
-export class UserEntity{
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+export class UserEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @Column()
-    username: string;
+  @Column()
+  username: string;
 
-    @Column({ unique: true })
-    email: string;
+  @Column({ unique: true })
+  email: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
 
-    
+  @Column({ nullable: true })
+  otp: string;
 
-    @Column({ nullable: true })
-    otp: string;
+  @Column({ nullable: true })
+  otpExpiration: Date;
 
-    @Column({ nullable: true })
-    otpExpiration: Date;
+  @ManyToMany(() => BranchEntity)
+  @JoinColumn({ name: "branchId" })
+  branch: BranchEntity;
 
-    
+  @Column({ nullable: true })
+  refreshToken: string;
+  @Column({
+    type: "enum",
+    enum: Role,
+    default: Role.ADMIN,
+  })
+  role: Role;
 
+  @Column({ nullable: true })
+  resetPasswordToken: string | null;
 
-    @Column({ nullable: true })
-    refreshToken: string;
-    @Column({
-        type: 'enum',
-        enum: Role,
-        default: Role.ADMIN
-    })
-    role: Role;
+  @Column({ nullable: true })
+  resetPasswordExpires: Date | null;
 
-    
-
-    @Column({ nullable: true })
-    resetPasswordToken: string | null;
-  
-    @Column({ nullable: true })
-    resetPasswordExpires: Date | null;
-
-
- // One user can have multiple notification devices (FCM tokens)
-  @OneToMany(() => NotificationEntity, notification => notification.user,{ cascade: true })
+  // One user can have multiple notification devices (FCM tokens)
+  @OneToMany(() => NotificationEntity, (notification) => notification.user, {
+    cascade: true,
+  })
   notifications: NotificationEntity[];
 
-  @OneToMany(() => FcmTokenEntity, FcmTokenEntity => FcmTokenEntity.user)
+  @OneToMany(() => FcmTokenEntity, (FcmTokenEntity) => FcmTokenEntity.user)
   fcmTokens: FcmTokenEntity[];
 }
