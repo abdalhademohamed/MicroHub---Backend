204e493374e9981df5ee39c6b570eace1a5c5e9a
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-14 16:28:42 +0300

    refactor : orders logic

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,12 +1,10 @@
 import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
-import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+
 import { InjectRepository } from '@nestjs/typeorm';
 import { OrderEntity } from './entities/order.entity';
 import { Repository } from 'typeorm';
 import { ReservationEntity } from '../reservation/entities/reservation.entity';
-import { CommentEntity } from '../comment/entities/comment.entity';
-import { CloudinaryService } from '../cloudinary/cloudinary.service';
+
 import { EmployeeEntity } from '../employee/entities/employee.entity';
 import { FindOrdersDto } from './dto/find.all.orders.dto';
 import { OrderStatus } from './utils/order.status.enum';
