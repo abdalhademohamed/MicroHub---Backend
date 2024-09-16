5bbc83874f8fc00152369fadeb53bee6365f5974
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-16 16:34:57 +0300

    refactor : receipt entity

--- src/receipt/entities/receipt.entity.ts
+++ src/receipt/entities/receipt.entity.ts
@@ -1,52 +1,58 @@
-import { OrderEntity } from '../../orders/entities/order.entity';
-import { ServiceEntity } from '../../service/entities/service.entity';
-import { UserEntity } from '../../user/entities/user.entity';
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, ManyToMany, JoinTable } from 'typeorm';
+import { OrderEntity } from "../../orders/entities/order.entity";
+import { ServiceEntity } from "../../service/entities/service.entity";
+import { UserEntity } from "../../user/entities/user.entity";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+  ManyToMany,
+  JoinTable,
+} from "typeorm";
 
 @Entity()
 export class ReceiptEntity {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
-  @ManyToOne(() => OrderEntity, OrderEntity => OrderEntity.receipts)
+  @ManyToOne(() => OrderEntity, (OrderEntity) => OrderEntity.receipts)
   order: OrderEntity;
 
-
   // Date when the receipt is generated
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
   generatedAt: Date;
 
   // Reservation time slot (can be a timestamp or a string depending on your implementation)
-  @Column({ type: 'varchar', length: 100 })
+  @Column({ type: 'varchar', length: 255 }) // Increase length as needed
   reservationTimeSlot: string;
+  
 
   // A message written on the receipt
-  @Column({ type: 'text', nullable: true })
+  @Column({ type: "text", nullable: true })
   message: string;
 
   // Total payment for the order
-  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  @Column({ type: "decimal", precision: 10, scale: 2 })
   totalPayment: number;
 
-  // Payment for services (can be an array of service names or a string listing the services)
-  @Column({ type: 'text' })
-  paymentForServices: string;
+  @Column('text') // Change from 'varchar(100)' to 'text'
+  paymentForServices: {
+    name: string;
+    duration: number;
+    price: string;
+  }[];
 
   // Discount applied to the order
-  @Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+  @Column({ type: "decimal", precision: 10, scale: 2, default: 0 })
   discount: number;
 
   // Remaining balance after the discount
-  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  @Column({ type: "decimal", precision: 10, scale: 2 })
   remaining: number;
 
   // Created by user reference
   @ManyToOne(() => UserEntity, { nullable: true })
   createdBy: UserEntity;
 
-  
-
-  @ManyToMany(() => ServiceEntity, ServiceEntity => ServiceEntity.receipts)
-  @JoinTable()
-  services: ServiceEntity[];
 }
