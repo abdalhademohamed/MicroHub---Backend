217543f4d255fa72be3791ca762f40f84421bc15
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-22 16:05:48 +0300

    refactor : branch dto

---
+++ src/branch/dto/employee.dto.ts
@@ -0,0 +1,34 @@
+import { IsString, IsOptional, IsNumber } from 'class-validator';
+
+export class EmployeeDto {
+  @IsString()
+  id: string;
+
+  @IsString()
+  username: string;
+
+  @IsString()
+  email: string;
+
+  @IsString()
+  role: string;
+
+  @IsString()
+  englishName: string;
+
+  @IsString()
+  arabicName: string;
+
+  @IsString()
+  workingHours: string;
+
+  @IsString()
+  phoneNumber: string;
+
+  @IsString()
+  image: string;
+
+  @IsOptional()
+  @IsNumber()
+  totalReview?: number; // Ensure this is a number
+}
