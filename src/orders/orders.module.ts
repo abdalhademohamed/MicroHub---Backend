815617c15bf705b5e60711ec625934bcaf751ce3
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-09-23 20:14:10 +0300

    refactor : order module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
@@ -12,38 +11,11 @@ import { ReceiptEntity } from '../receipt/entities/receipt.entity';
 import { UserEntity } from '../user/entities/user.entity';
 import { AuditLogEntity } from '../audit-log/entities/audit.log.entity';
 import { PaymentEntity } from '../payment/entities/payment.entity';
-import { PositionEntity } from 'src/postion/entities/postion.entity';
+import { PositionEntity } from '../postion/entities/postion.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([OrderEntity, CommentEntity, ReservationEntity,EmployeeEntity,ReceiptEntity,UserEntity,PaymentEntity,PositionEntity]),
-=======
-import { Module } from "@nestjs/common";
-import { OrdersService } from "./orders.service";
-import { OrdersController } from "./orders.controller";
-import { OrderEntity } from "./entities/order.entity";
-import { CommentEntity } from "../comment/entities/comment.entity";
-import { ReservationEntity } from "../reservation/entities/reservation.entity";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { CloudinaryService } from "../cloudinary/cloudinary.service";
-import { EmployeeEntity } from "../employee/entities/employee.entity";
-import { ReceiptEntity } from "../receipt/entities/receipt.entity";
-import { UserEntity } from "../user/entities/user.entity";
-import { AuditLogEntity } from "../audit-log/entities/audit.log.entity";
-import { PaymentEntity } from "../payment/entities/payment.entity";
-
-@Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      OrderEntity,
-      CommentEntity,
-      ReservationEntity,
-      EmployeeEntity,
-      ReceiptEntity,
-      UserEntity,
-      PaymentEntity,
-    ]),
->>>>>>> bbfc03ec90e6938aca7a5731ab27823b36223600
   ],
   controllers: [OrdersController],
   providers: [OrdersService, CloudinaryService],
