76edcb9e0cfefc93fd476bfa19e2907f5ea75762
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-10-24 12:48:51 +0300

    refactor : order

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1246,7 +1246,7 @@ export class OrdersService {
           await transactionalEntityManager.save(AuditLogEntity, log);
           if (order.status === OrderStatus.Canceled) {
 
-            await this.reservationService.cancelReservationAndAddSlot(order.reservation.start_Time,order.reservation.end_Time,order.branch.id)
+            await this.reservationService.deleteReservation(order.id)
             const usersToNotify = await transactionalEntityManager.find(
               UserEntity,
               {
