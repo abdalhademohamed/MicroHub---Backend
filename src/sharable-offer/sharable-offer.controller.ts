04bb4eb08952509f576697ecbada5dcbef9dae17
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-10-30 12:25:01 +0300

    refactor : sharable offer RBAC

--- src/sharable-offer/sharable-offer.controller.ts
+++ src/sharable-offer/sharable-offer.controller.ts
@@ -56,14 +56,14 @@ export class SharableOfferController {
 
 
 @UseGuards(AccessTokenGuard, RolesGuard) // Ensure AccessTokenGuard is first
-  @Roles(Role.SUPERADMIN,Role.ACCOUNTANT)
+  @Roles(Role.SUPERADMIN,Role.ACCOUNTANT,Role.COORDINATOR)
   @Get('active') // Route to get active sharable offers
   async findActiveSharableOffer(): Promise<SharableOfferEntity[]> {
     return await this.sharableOfferService.findActiveSharableOffer();
   }
 
   @UseGuards(AccessTokenGuard, RolesGuard) // Ensure AccessTokenGuard is first
-  @Roles(Role.SUPERADMIN,Role.ACCOUNTANT)
+  @Roles(Role.SUPERADMIN,Role.ACCOUNTANT,Role.COORDINATOR)
   @Get('count') // GET request to /sharable-offers/count
   @ApiResponse({ status: 200, description: 'Get the count of sharable offers.' })
   async getSharableOffersCount(): Promise<{ total: number; active: number }> {
@@ -73,7 +73,7 @@ export class SharableOfferController {
 
 
 @UseGuards(AccessTokenGuard, RolesGuard) // Ensure AccessTokenGuard is first
-  @Roles(Role.SUPERADMIN,Role.ACCOUNTANT)
+  @Roles(Role.SUPERADMIN,Role.ACCOUNTANT,Role.COORDINATOR)
   @Get() // GET request to /sharable-offers
   async getAllSharableOffers(): Promise<SharableOfferEntity[]> {
     return await this.sharableOfferService.findAllSharableOffers();
