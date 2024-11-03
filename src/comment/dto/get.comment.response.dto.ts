e0d1cc438acae94dfa28a7ea015bbfb8c522010f
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-11-03 14:11:58 +0200

    refactor : comment

---
+++ src/comment/dto/get.comment.response.dto.ts
@@ -0,0 +1,49 @@
+// comment-response.dto.ts
+export class ReviewResponseDto {
+    id: string;
+    rating: number;
+    createdAt: Date;
+    imageOrder: string;
+    commentBefore?: string;
+    commentAfter?: string;
+    reviewer: {
+      id: string;
+      username: string;
+      english_Name: string;
+      role: string;
+      phoneNumber: string;
+    } | null;
+    artist: {
+      id: string;
+      username: string;
+      english_Name: string;
+      role: string;
+      phoneNumber: string;
+    } | null;
+  }
+  
+  export class CommentResponseDto {
+    id: string;
+    content: string;
+    imageBeforeUrl: string;
+    imageAfterUrl: string;
+    createdAt: Date;
+    employee: {
+      id: string;
+      username: string;
+      email: string;
+      role: string;
+      english_Name: string;
+      arabic_Name: string;
+      workingHours: string;
+      phoneNumber: string;
+      image: string;
+      available: boolean;
+      totalReviews: number;
+      status: string;
+      oldestAvgRating: number;
+      newestAvgRating: number;
+    } | null;
+    reviews: ReviewResponseDto[];
+  }
+  
