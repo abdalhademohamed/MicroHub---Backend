d5c890013c4f1b6c99e5ff9d355d400912d8cd6b
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-11-05 14:39:07 +0200

    refactor : localization

--- src/common/custom.18n.service.ts
+++ src/common/custom.18n.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from "@nestjs/common";
-import { I18nContext, I18nService } from "nestjs-i18n";
+import { Injectable, Scope, Inject } from "@nestjs/common";
+import { I18nService } from "nestjs-i18n";
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class CustomI18nService {
-  constructor(private I18nService: I18nService) {}
+  constructor(
+    private i18nService: I18nService,
+    @Inject(REQUEST) private request: Request
+  ) {}
 
   translate(key: string, options?: any) {
-    const lang = I18nContext.current().lang;
-    return this.I18nService.t(key, { lang, ...options });
+    const lang = this.request.headers['lang'] as string || 'en';
+    return this.i18nService.t(key, { lang, ...options });
   }
 }
+ 
