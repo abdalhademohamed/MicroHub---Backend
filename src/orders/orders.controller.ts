9adb82b562397461eef4c308c38653b8a6b5df9f
Mahmoud Ammar <mahmoud.ammar560@gmail.com>
2024-11-06 18:12:43 +0200

    refactor : order

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -195,7 +195,7 @@ export class OrdersController {
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   @UseGuards(AccessTokenGuard, RolesGuard)
-  @Roles(Role.SUPERADMIN, Role.COORDINATOR, Role.RECEPTIONIST,Role.ARTISTMANAGER,Role.ACCOUNTANT)
+  @Roles(Role.SUPERADMIN, Role.COORDINATOR, Role.RECEPTIONIST,Role.ARTISTMANAGER,Role.ACCOUNTANT,Role.ARTIST,Role.BRANCHMANAGER)
   @Get('status/count')
   async getOrderStatusCount(
     @Query('branchId') branchId?: string,
